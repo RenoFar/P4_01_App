64628871b0000d9ea1da1f63ccc23d3716d864d9
RenoFar <renaudfaraci@gmail.com>
2020-11-13 14:01:49 +0100

    annotation de la structure de la fonction principale

--- Main.py
+++ Main.py
@@ -3,7 +3,8 @@
 
 import Package.fonctions
 
-class Tournoi: # Définition de notre classe Tournoi
+
+class Tournoi:  # Définition de notre classe Tournoi
     """Classe définissant un tournoi caractérisé par :
     - son nom
     - son lieu
@@ -14,18 +15,17 @@ class Tournoi: # Définition de notre classe Tournoi
     - son controleur du temps
     - sa descrition"""
 
-    def __init__(self):  # Notre méthode constructeur
-        """Pour l'instant, on ne va définir que les attribut"""
-        self.nom = ""
-        self.lieu = ""
-        self.date= ""
-        self.nbre_tour = 1
-        self.tournee = []
-        self.indices_joueurs = []
-        self.description = ""
+    def __init__(self, nom="", lieu="", date="", nbre_tour=4, tournee=[], indices_joueurs=[], description=""):
+        self.nom = nom
+        self.lieu = lieu
+        self.date = date
+        self.nbre_tour = nbre_tour
+        self.tournee = tournee
+        self.indices_joueurs = indices_joueurs
+        self.description = description
 
 
-class Joueur: # Définition de notre classe Joueur
+class Joueur:  # Définition de notre classe Joueur
     """Classe définissant un joueur caractérisé par :
     - son nom
     - son prénom
@@ -33,26 +33,34 @@ class Joueur: # Définition de notre classe Joueur
     - son sexe
     - sa classement"""
 
-    def __init__(self):  # Notre méthode constructeur
-        """Pour l'instant, on ne va définir que les attribut"""
-        self.nom = ""
-        self.prenom = ""
-        self.date_naissance = ""
-        self.sexe = ""
-        self.classement = 1
+    def __init__(self, nom="", prenom="", date_naissance="", sexe="", classement=1):
+        self.nom = nom
+        self.prenom = prenom
+        self.date_naissance = date_naissance
+        self.sexe = sexe
+        self.classement = classement
 
 
-class Tour: # Définition de notre classe Tour
+class Tour:  # Définition de notre classe Tour
     """Classe définissant un tour caractérisé par :
     - sa liste de matchs"""
 
-    def __init__(self):  # Notre méthode constructeur
-        """Pour l'instant, on ne va définir que les attribut"""
+    def __init__(self):
         self.liste_matchs = []
 
 
 def main():
     pass
+#créer tournoi
+    #Ajouter huit joueurs
+    #executer les tours
+        #déterminer la liste des matchs
+            #générer les paires de joueurs (instance de ronde)
+        #lancer les matchs
+            #entrer les résultats
+        #sauvegarder le controleur de temps
+
+
 
 if __name__ == "__main__":
     main()
