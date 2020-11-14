04364bb94efdceef51e06982389ae80429a3aaad
RenoFar <renaudfaraci@gmail.com>
2020-11-14 16:02:06 +0100

    création menu

--- Main.py
+++ Main.py
@@ -15,15 +15,15 @@ class Tournoi:  # Définition de la classe Tournoi
     - son controleur de temps
     - sa descrition"""
 
-    def __init__(self, nom="", lieu="", date="", nbre_tour=4, description=""):
+    def __init__(self, nom, lieu, date, nbre_tour=4, mode_jeu, description=""):
         self.nom = nom
         self.lieu = lieu
         self.date = date
         self.nbre_tour = nbre_tour
+        self.mode_jeu = mode_jeu
         self.description = description
-        """self.tournee = tournee"""
-        """self.indices_joueurs = indices_joueurs"""
-        """self.duree = duree"""
+        self.tournee = []
+        self.indices_joueurs = []
 
 
 class Joueur:  # Définition de la classe Joueur
@@ -33,66 +33,95 @@ class Joueur:  # Définition de la classe Joueur
     - sa date de naissance
     - son sexe
     - son classement
-    - Liste des indices correspondant aux instances du joueur"""
+    - numéro de l'indice joueur en mémoire"""
 
-    def __init__(self, nom="", prenom="", date_naissance="", sexe="", classement=1):
+    def __init__(self, nom, prenom, date_naissance, sexe, classement=0, indice=""):
         self.nom = nom
         self.prenom = prenom
         self.date_naissance = date_naissance
         self.sexe = sexe
         self.classement = classement
-        """"self.indices = indices"""
+        self.indice = indice
 
 
 class Tour:  # Définition de la classe Tour
     """Classe définissant un tour caractérisé par :
+    - son nom
+    - sa date
+    - son heure de début
+    - son heure de fin
     - sa liste de matchs"""
 
-    def __init__(self):
+    def __init__(self, nom, date, debut, fin):
+        self.nom = nom
+        self.date = date
+        self.debut = debut
+        self.fin = fin
         self.liste_matchs = []
 
 
 def creer_tournoi():  # créer le tournoi
-    tournoi = Tournoi()
-    while len(tournoi.nom) < 1 or not tournoi.nom.isalnum():
-        tournoi.nom = input("\nVeuillez saisir le nom du tournoi: ")
-    while len(tournoi.lieu) < 1:
-        tournoi.lieu = input("\n saisir le Lieu du tournoi: ")
-    while len(tournoi.date) < 1:
-        tournoi.date = input("\nDate du tournoi: ")
-    while len(tournoi.description) < 1:
-        tournoi.description = input("\nVeuillez saisir la Description du tournoi: ")
+    nom = "", lieu = "", date = "", nbre_tour = 4, mode_jeu, description = ""
+    while len(nom) < 1 or not nom.isalnum():
+        nom = input("\nVeuillez saisir le nom du tournoi: ")
+    while len(lieu) < 1:
+        lieu = input("Veuillez saisir le Lieu du tournoi: ")
+    while len(date) < 1:
+        date = input("Date du tournoi: ")
+    while len(description) < 1:
+        description = input("Veuillez saisir la Description du tournoi: ")
     while True:
-        tournoi.nbre_tour = input('\nle nombre de tours par défaut est de 4.'
+        nbre_tour = input('le nombre de tours par défaut est de 4.'
                                   '\nSaisissez un autre nombre ou Entrée pour valider: ')
-        if len(tournoi.nbre_tour) < 1:  # si rien n'est saisi
-            tournoi.nbre_tour = 4
+        if len(nbre_tour) < 1:  # si rien n'est saisi
+            nbre_tour = 4
             break
         else:
             try:
-                tournoi.nbre_tour = int(tournoi.nbre_tour)  # conversion en entier
-                if tournoi.nbre_tour > 0: break
-                except ValueError: tournoi.nbre_tour = 4  # reinitialise la valeur à 4
-    print('le nombre de tours est de ' + str(tournoi.nbre_tour))
+                nbre_tour = int(nbre_tour)  # conversion en entier
+                if nbre_tour > 0: break
+            except ValueError: print('Veuillez saisir un entier positif!\n')
+    print('le nombre de tours est de ' + str(nbre_tour))
+    while mode_jeu.lower() not in ('bullet', 'blitz', 'rapide'):
+        mode_jeu = input("Veuillez saisir le mode du tournoi (bullet / blitz / rapide): ")
+    tournoi = Tournoi(nom, lieu, date, nbre_tour, mode_jeu, description)
     return tournoi
 
 
-def Ajouter_joueur():  # Ajouter les joueurs
-    joueur = Joueur()
-    while len(joueur.nom) < 1 or not joueur.nom.isalnum():
-        joueur.nom = input("\nVeuillez saisir le nom du joueur: ")
-    while len(joueur.prenom) < 1 or not joueur.prenom.isalnum():
-        joueur.prenom = input("\nVeuillez saisir le prénom du joueur: ")
+def ajouter_joueur():  # ajouter les joueurs
+    ajout = ""
+    while ajout not in ('1', '2'):
+        ajout = input('\nAjouter un joueur connu : 1'
+                      '\nAjouter un nouveau joueur : 2')
+    if ajout == '1':
 
-    joueur.date_naissance = input("\nVeuillez saisir la date de naissance du joueur: ")
+    if ajout == '2':
+        creer_joueur_()
 
-    while joueur.sexe.lower() != 'f' or joueur.sexe.lower() != 'm':
-        joueur.sexe = input("\nVeuillez saisir le sexe du joueur (F/M): ")
-    while len(joueur.classement) < 1 or not joueur.classement.isalnum():
-        joueur.classement = input("\nVeuillez saisir le classement du joueur: ")
+
+def creer_joueur_:()
+    nom = "", prenom = "", date_naissance ="", sexe ="", classement = 0, indice = ""
+    while len(nom) < 1 or not nom.isalnum():
+        nom = input("\nVeuillez saisir le nom du joueur: ")
+    while len(prenom) < 1 or not prenom.isalnum():
+        prenom = input("Veuillez saisir le prénom du joueur: ")
+
+    date_naissance = input("Veuillez saisir la date de naissance du joueur: ")
+
+    while sexe.lower() not in ('f', 'm'):
+        sexe = input("Veuillez saisir le sexe du joueur (F/M): ")
+    while true:
+        classement = input("Veuillez saisir le classement du joueur: ")
+        try
+            classement = int(classement)
+            if classement > 0 : break
+        except ValueError: print('Veuillez saisir un entier positif!\n')
+    joueur = Joueur(nom, prenom, date_naissance, sexe, classement, indice)
     return joueur
 
+
     # déterminer la liste des matchs
+
     # générer les paires de joueurs (instance de ronde)
     # lancer les matchs
     # entrer les résultats
@@ -101,7 +130,7 @@ def Ajouter_joueur():  # Ajouter les joueurs
     # afficher les résultats
 
 
-# Générer des rapports
+# générer des rapports
 # lister les acteurs
 # lister les joueurs du tournoi
 # lister les tournois
@@ -110,10 +139,17 @@ def Ajouter_joueur():  # Ajouter les joueurs
 
 
 def main():
-    """"Fonction principale d'exécution de l'application"""
-    creer_tournoi()
+    """Fonction principale d'exécution de l'application"""
+choix =""
+while choix not in ('1', '2'):
+    choix = input('\nMenu principal: '
+                  '\nCréer un tournoi : 1'
+                  '\nVoir les raports : 2')
+if choix == '1':
+    choix1 = creer_tournoi()
+    ajouter_joueur()
+if choix == '2': pass
 
-    pass
 
 
 if __name__ == "__main__":
