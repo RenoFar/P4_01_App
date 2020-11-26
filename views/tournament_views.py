2416782d32da350817352a07eef67ae5b6ed770a
RenoFar <renaudfaraci@gmail.com>
2020-11-26 12:56:16 +0100

    formating tournament_views.py with f-string

--- views/tournament_views.py
+++ views/tournament_views.py
@@ -3,16 +3,16 @@
 
 
 def input_data(info, line_break_start='', line_line_break_end=''):
-    return input(line_break_start + info + line_line_break_end)
+    return input(f'{line_break_start}{info}{line_line_break_end}')
 
 
 def print_info(info, line_break_start='', line_line_break_end=''):
-    print(line_break_start + info + line_line_break_end)
+    print(f'{line_break_start}{info}{line_line_break_end}')
 
 
 def print_menu(info, line_break_start='', line_line_break_end=''):
-    print(line_break_start + ' --------- ' + info + ' --------- ' + line_line_break_end)
+    print(f'{line_break_start} --------- {info} --------- {line_line_break_end}')
 
 
 def print_board(info1, info2, info3='', line_break_start='', line_line_break_end=''):
-    print(line_break_start + 'Player {} ranked {} {}'.format(info1, info2, info3) + line_line_break_end)
+    print(f'{line_break_start}Player {info1} ranked {info2} {info3}{line_line_break_end}')
